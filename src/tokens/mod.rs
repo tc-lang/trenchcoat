04275a14938d40eb60446bf77f57d7bbd9260a6b
sharnoff <github@max.sharnoff.org>
2020-04-26 23:51:50 +0100

    Rename `Token` to `TokenKind`, add `Token` composed of kind + byte index
    Also other miscellaneous changes thrown in as well. Most of them are to
    do with refining what's already here - primarily with the goal of
    improving readability and (intentionally) limiting scope.

--- src/tokens/mod.rs
+++ src/tokens/mod.rs
@@ -1,74 +1,32 @@
 mod position;
 mod reader;
 
-pub fn tokenize(s: &str) -> (Vec<(usize, Token)>, usize) {
+pub fn tokenize(s: &str) -> (Vec<Token>, usize) {
     Token::parse(|_| false, s)
 }
 
 #[derive(Debug)]
-pub enum Token<'a> {
+pub struct Token<'a> {
+    /// The starting position of the token in the bytes of the file
+    byte_idx: usize,
+
+    /// The content of the token
+    kind: TokenKind<'a>,
+}
+
+#[derive(Debug)]
+pub enum TokenKind<'a> {
     Keyword(Keyword),
     TypeIdent(&'a str),
     NameIdent(&'a str),
     Oper(Oper),
     Punc(Punc),
     Num(&'a str),
-    Parens(Vec<(usize, Token<'a>)>),
-    Curlys(Vec<(usize, Token<'a>)>),
-    Squares(Vec<(usize, Token<'a>)>),
+    Parens(Vec<Token<'a>>),
+    Curlys(Vec<Token<'a>>),
+    Squares(Vec<Token<'a>>),
     InvalidChar(char),
 }
-
-/*
-impl Token {
-    /// fmt_write writes "kind(value) " to the formatter.
-    fn fmt_write(f: &mut fmt::Formatter, kind: &str, value: &str) -> Result<(), fmt::Error> {
-        f.write_str(kind)?;
-        f.write_str("(")?;
-        f.write_str(&*value)?;
-        f.write_str(") ")
-    }
-
-    /// fmt_vec calls `t.fmt(f)` for each t in ts.
-    fn fmt_vec(f: &mut fmt::Formatter, ts: &[(Position, Token)]) -> Result<(), fmt::Error> {
-        for (_, t) in ts {
-            t.fmt(f)?;
-        }
-        Ok(())
-    }
-}
-
-impl std::fmt::Debug for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        use Token::*;
-        match self {
-            Keyword(kw) => Self::fmt_write(f, Keyword, fmt!("{:?}", kw)),
-            TypeIdent(s) => Self::fmt_write(f, "Type", s),
-            NameIdent(s) => Self::fmt_write(f, "Name", s),
-            Oper(s) => Self::fmt_write(f, "Op", fmt!("{:?}", kw)),
-            Punc(s) => {
-                if s == "\n" {
-                    f.write_str("\n")
-                } else {
-                    Self::fmt_write(f, "Punc", s)
-                }
-            }
-            Num(s) => Self::fmt_write(f, "Num", s),
-            Bracketed(ts) => {
-                f.write_str("(")?;
-                Self::fmt_vec(f, ts)?;
-                f.write_str(") ")
-            }
-            Curlied(ts) => {
-                f.write_str("{")?;
-                Self::fmt_vec(f, ts)?;
-                f.write_str("} ")
-            }
-            InvalidChar(_) => f.write_str("-INVALID CHAR-"),
-        }
-    }
-}
-*/
 
 #[derive(Debug)]
 pub enum Keyword {
@@ -76,7 +34,6 @@ pub enum Keyword {
     If,
     Let,
     Type,
-    Match,
     Return,
 }
 
@@ -88,7 +45,6 @@ impl Keyword {
             "if" => Some(If),
             "let" => Some(Let),
             "type" => Some(Type),
-            "match" => Some(Match),
             "return" => Some(Return),
             _ => None,
         }
@@ -99,13 +55,10 @@ impl Keyword {
 pub enum Oper {
     Add,
     Sub,
-    Astrix,
+    Star,
     Div,
-    Pow,
-    Inc,
-    Dec,
-    Question,
     Ref,
+    DotDot,
     Assign,
     Equals,
     LT,
@@ -114,7 +67,6 @@ pub enum Oper {
     GTOrEqual,
     Not,
     RightArrow,
-    LeftArrow, // they're the wrong way round, deal with it.
     FuncArrow,
     Or,
     And,
@@ -126,13 +78,10 @@ impl Oper {
         match oper {
             "+" => Some(Add),
             "-" => Some(Sub),
-            "*" => Some(Astrix),
-            "**" => Some(Pow),
+            "*" => Some(Star),
             "/" => Some(Div),
-            "++" => Some(Inc),
-            "--" => Some(Dec),
             "&" => Some(Ref),
-            "?" => Some(Question),
+            ".." => Some(DotDot),
             "=" => Some(Assign),
             "==" => Some(Equals),
             "<" => Some(LT),
@@ -141,7 +90,6 @@ impl Oper {
             ">=" => Some(GTOrEqual),
             "!" => Some(Not),
             "->" => Some(RightArrow),
-            "<-" => Some(LeftArrow),
             "=>" => Some(FuncArrow),
             "||" => Some(Or),
             "&&" => Some(And),
@@ -150,18 +98,21 @@ impl Oper {
     }
 }
 
-///returns true if `ch` is an operator character.
+/// returns true if `ch` is an operator character.
 fn is_oper(ch: char) -> bool {
-    (match ch {
-        '+' | '-' | '=' | '/' | '*' | '.' | '<' | '>' => true,
-        _ => false,
-    }) || is_single_oper(ch) // it's a shame the match has to be in brackets :(
+    is_single_oper(ch)
+        || match ch {
+            '+' | '-' | '=' | '/' | '*' | '.' | '<' | '>' => true,
+            _ => false,
+        }
 }
-///returns true if `ch` is a 'single' operator character. A single operator char is one where
-///multiple characters next to eachother compounds the operator rather than referring to a
-///different operator.
-///For example, `+` is not a single operator char since it is distinct from `++` whereas
-///             `!` is a single operator char since `!!x` is equivilent to `!(!x)`.
+
+/// returns true if `ch` is a 'single' operator character. A single operator char is one where
+/// multiple characters next to eachother compounds the operator rather than referring to a
+/// different operator.
+///
+/// For example, `+` is not a single operator char since it is distinct from `++` whereas `!` is a
+/// single operator char since `!!x` is equivilent to `!(!x)`.
 fn is_single_oper(ch: char) -> bool {
     match ch {
         '?' | '!' | '&' | '*' => true,
@@ -173,59 +124,48 @@ fn is_single_oper(ch: char) -> bool {
 pub enum Punc {
     Dot,
     Comma,
-    //Dot? This would have to be added as a special case to the operator parser
     Colon,
-    SemiColon,
+    Semi,
 }
 
 impl Punc {
     fn parse(punc: &str) -> Option<Punc> {
         use Punc::*;
         match punc {
+            "." => Some(Dot),
             "," => Some(Comma),
             ":" => Some(Colon),
-            ";" => Some(SemiColon),
+            ";" => Some(Semi),
             _ => None,
         }
     }
 }
 
 fn is_whitespace(ch: char) -> bool {
-    ch == ' ' || ch == '\t'
+    ch == ' ' || ch == '\t' || ch == '\n'
 }
 
 fn is_special_type(s: &str) -> bool {
-    match s {
-        "int" | "uint" | "bool" | "i8" | "u8" | "i16" | "u16" | "i32" | "u32" | "i64" | "u64"
-        | "f32" | "f64" => true,
-        _ => false,
-    }
+    // Currently, we're only supporting `uint`s
+    s == "uint"
 }
 
 fn is_punc(ch: char) -> bool {
-    (match ch {
-        ':' => true,
-        _ => false,
-    }) || is_single_punc(ch)
-}
-
-/// for the distinction between single chars and normal chars, see the documentation for
-/// `is_single_oper`.
-fn is_single_punc(ch: char) -> bool {
     match ch {
-        '\n' | ',' | ';' => true,
+        '.' | ',' | ':' | ';' => true,
         _ => false,
     }
 }
 
-fn first_char_from(s: &str, i: usize) -> Option<(char, usize)> {
-    let mut ci = s.get(i..)?.char_indices();
-    let (_, c) = ci.next()?;
-    let (j, _) = ci.next().unwrap_or((s.len(), '0'));
-    Some((c, i + j))
+/// Produces the first character in `s[idx..]`, paired with its byte index in `s`
+fn first_char_from(s: &str, idx: usize) -> Option<(char, usize)> {
+    let mut char_idxs = s.get(idx..)?.char_indices();
+    let (_, ch) = char_idxs.next()?;
+    let (ch_len, _) = char_idxs.next().unwrap_or((s.len() - idx, '0'));
+    Some((ch, idx + ch_len))
 }
 
-impl Token<'_> {
+impl TokenKind<'_> {
     /// consume forms the basis for most token parsing.
     /// It consumes a token described by `start`, `mid` and `term` from `s`.
     /// The token will:
@@ -242,11 +182,13 @@ impl Token<'_> {
         if !start(c) {
             return None;
         }
+
         if !term(c) {
             while let Some((c, j)) = first_char_from(s, i) {
                 if !mid(c) {
                     break;
                 }
+
                 i = j;
                 if term(c) {
                     break;
@@ -256,34 +198,34 @@ impl Token<'_> {
         return Some((&s[..i], i));
     }
 
-    /// parses a `Token::Oper`
-    fn oper(s: &str) -> Option<(Token, usize)> {
+    /// parses a `TokenKind::Oper`
+    fn oper(s: &str) -> Option<(TokenKind, usize)> {
         let (oper_str, i) = Self::consume(is_oper, is_oper, is_single_oper, s)?;
         if oper_str == "." {
-            return Some((Token::Punc(Punc::Dot), i));
+            return Some((TokenKind::Punc(Punc::Dot), i));
         }
-        Some((Token::Oper(Oper::parse(oper_str)?), i))
+        Some((TokenKind::Oper(Oper::parse(oper_str)?), i))
     }
 
-    /// parses a `Token::Num`
-    fn num(s: &str) -> Option<(Token, usize)> {
+    /// parses a `TokenKind::Num`
+    fn num(s: &str) -> Option<(TokenKind, usize)> {
         let (n, i) = Self::consume(
             |c| c.is_ascii_digit(),
             |c| c.is_alphanumeric() || c == '_',
             |_| false,
             s,
         )?;
-        Some((Token::Num(n), i))
+        Some((TokenKind::Num(n), i))
     }
 
-    /// parses a `Token::Punc`
-    fn punc(s: &str) -> Option<(Token, usize)> {
-        let (punc_str, i) = Self::consume(is_punc, is_punc, is_single_punc, s)?;
-        Some((Token::Punc(Punc::parse(punc_str)?), i))
+    /// parses a `TokenKind::Punc`
+    fn punc(s: &str) -> Option<(TokenKind, usize)> {
+        let (punc_str, i) = Self::consume(is_punc, is_punc, is_punc, s)?;
+        Some((TokenKind::Punc(Punc::parse(punc_str)?), i))
     }
 
-    /// parses a `Token::NameIdent`, `Token::TypeIdent` or `Token::Keyword`
-    fn name(s: &str) -> Option<(Token, usize)> {
+    /// Attempts to parse any of `TokenKind::{NameIdent, TypeIdent, Keyword}`
+    fn name(s: &str) -> Option<(TokenKind, usize)> {
         let (name, i) = Self::consume(
             |c| c.is_ascii_lowercase(),
             |c| c.is_ascii_alphanumeric() || c == '_',
@@ -292,47 +234,50 @@ impl Token<'_> {
         )?;
         Some((
             if let Some(keyword) = Keyword::parse(name) {
-                Token::Keyword(keyword)
+                TokenKind::Keyword(keyword)
             } else if is_special_type(name) {
-                Token::TypeIdent(name)
+                TokenKind::TypeIdent(name)
             } else {
-                Token::NameIdent(name)
+                TokenKind::NameIdent(name)
             },
             i,
         ))
     }
-    fn typ(s: &str) -> Option<(Token, usize)> {
+    fn typ(s: &str) -> Option<(TokenKind, usize)> {
         let (ident, i) = Self::consume(
             |c| c.is_ascii_uppercase(),
             |c| c.is_ascii_alphanumeric() || c == '_',
             |_| false,
             s,
         )?;
-        Some((Token::TypeIdent(ident), i))
+        Some((TokenKind::TypeIdent(ident), i))
     }
 
     /// parses a `Token::Bracketed` or a `Token::Curlied`
-    fn block(s: &str) -> Option<(Token, usize)> {
+    fn block(s: &str) -> Option<(TokenKind, usize)> {
         let (c, i) = first_char_from(s, 0)?;
         match c {
             '(' => {
-                let (blk, i) = Self::parse(|c| c == ')', s.get(i..)?);
-                Some((Token::Parens(blk), i))
+                let (blk, i) = Token::parse(|c| c == ')', s.get(i..)?);
+                Some((TokenKind::Parens(blk), i))
             }
             '{' => {
-                let (blk, i) = Self::parse(|c| c == '}', s.get(i..)?);
-                Some((Token::Curlys(blk), i))
+                let (blk, i) = Token::parse(|c| c == '}', s.get(i..)?);
+                Some((TokenKind::Curlys(blk), i))
             }
             '[' => {
-                let (blk, i) = Self::parse(|c| c == ']', s.get(i..)?);
-                Some((Token::Squares(blk), i))
+                let (blk, i) = Token::parse(|c| c == ']', s.get(i..)?);
+                Some((TokenKind::Squares(blk), i))
             }
             _ => None,
         }
     }
-    /// Parses 1 token from s. This discards leading whitespace. If stop returns true for the first
-    /// non-whitespace character then it is consumed and None is returned.
-    fn parse_next(stop: impl Fn(char) -> bool, s: &str) -> (Option<(usize, Token)>, usize) {
+
+    /// Parses a single token from `s`, discarding leading whitespace if any exists
+    ///
+    /// Iff `stop` returns true for the first non-whitespace character, it is consumed and `None`
+    /// is returned.
+    fn parse_next(stop: impl Fn(char) -> bool, s: &str) -> (Option<(usize, TokenKind)>, usize) {
         let mut i = 0;
         loop {
             if let Some((c, j)) = first_char_from(s, i) {
@@ -347,21 +292,27 @@ impl Token<'_> {
                 return (None, i);
             }
         }
+
         if let Some(s) = s.get(i..) {
             if let Some((t, l)) = Self::num(s)
                 .or_else(|| Self::name(s))
                 .or_else(|| Self::typ(s))
+                // We do operators before punctuation because '..' is an operator that could
+                // alternatively be seen as two of the punctuation '.', which is not the correct
+                // way of parsing it.
+                .or_else(|| Self::oper(s))
                 .or_else(|| Self::punc(s))
                 .or_else(|| Self::block(s))
-                .or_else(|| Self::oper(s))
-                .or_else(|| first_char_from(s, 0).map(|(c, i)| (Token::InvalidChar(c), i)))
+                .or_else(|| first_char_from(s, 0).map(|(c, i)| (TokenKind::InvalidChar(c), i)))
             {
                 return (Some((i, t)), i + l);
             }
         }
         return (None, i);
     }
+}
 
+impl Token<'_> {
     /// Takes a Reader of chars and parses it to tokens.
     /// We might could have this return Iterator<Token>? Not sure how much of a benifit that
     /// would be.
@@ -370,13 +321,13 @@ impl Token<'_> {
     fn parse(
         stop: impl Fn(char) -> bool + Copy, /* Max, what's best here? +Copy or passing a reference around? */
         s: &str,
-    ) -> (Vec<(usize, Token)>, usize) {
+    ) -> (Vec<Token>, usize) {
         let mut out = Vec::with_capacity(4096);
         let mut i = 0;
-        while let Some((op, l)) = s.get(i..).map(|ss| Self::parse_next(stop, ss)) {
+        while let Some((op, l)) = s.get(i..).map(|ss| TokenKind::parse_next(stop, ss)) {
             i += l;
-            if let Some(p) = op {
-                out.push(p);
+            if let Some((byte_idx, kind)) = op {
+                out.push(Token { byte_idx, kind });
             } else {
                 break;
             }
