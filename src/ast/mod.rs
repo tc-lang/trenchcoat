ff2e06bf320775ea03de92d5e959579ef55523f4
Jacob O'Toole <jacob.otoole1@gmail.com>
2020-04-28 15:00:30 +0100

    Updated try_parse to allow for Punc::Sep between top-level declarations.
    We should probably enforce this?

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -35,16 +35,20 @@ pub fn try_parse<'a>(tokens: &'a [Token<'a>]) -> Result<Vec<Item<'a>>, Vec<Error
     let mut idx = 0;
 
     while idx < tokens.len() {
-        match Item::parse_top_level(&tokens[idx..]) {
-            ParseRet::Err(err) => return Err(err),
-            ParseRet::SoftErr(item, err) => {
-                idx += item.consumed();
-                items.push(item);
-                errors.extend(err);
-            }
-            ParseRet::Ok(item) => {
-                idx += item.consumed();
-                items.push(item);
+        if tokens[idx].kind == TokenKind::Punc(Punc::Sep) {
+            idx += 1;
+        } else {
+            match Item::parse_top_level(&tokens[idx..]) {
+                ParseRet::Err(err) => return Err(err),
+                ParseRet::SoftErr(item, err) => {
+                    idx += item.consumed();
+                    items.push(item);
+                    errors.extend(err);
+                }
+                ParseRet::Ok(item) => {
+                    idx += item.consumed();
+                    items.push(item);
+                }
             }
         }
     }
