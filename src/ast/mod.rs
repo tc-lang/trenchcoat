tc-lang/trenchcoat

commit 98b4fb82d99b210b1695cca8536f07a5df51d73e

Add AST error messages

src/ast/mod.rs
@@ -664,7 +664,7 @@ impl<'a> Block<'a> {
                     //
                     // If the expression *is* valid, we'll return because it's the last thing - we
                     // can do this by simply breaking out of the loop
-                    match Expr::parse(&tokens[idx..]) {
+                    match Expr::parse(&tokens[idx..], true) {
                         // expression parsing failed, so we'll go with the original statement
                         // parsing error and return
                         None | Some(ParseRet::Err(_)) => {
@@ -1296,7 +1296,7 @@ impl<'a> Stmt<'a> {
             .find(|(_, t)| t.kind == Punc(Sep))
             .map(|(i, _)| i)?;
 
-        Expr::parse(&tokens[..sep_idx]).map(|ret| match ret {
+        Expr::parse(&tokens[..sep_idx], false).map(|ret| match ret {
             ParseRet::Ok(expr) => ParseRet::Ok(expr),
             ParseRet::SoftErr(expr, errs) => ParseRet::SoftErr(
                 expr,
@@ -1320,13 +1320,18 @@ impl<'a> Expr<'a> {
         self.source.len()
     }
 
-    fn parse(tokens: &'a [Token<'a>]) -> Option<ParseRet<'a, Expr<'a>>> {
+    fn parse(tokens: &'a [Token<'a>], allow_empty: bool) -> Option<ParseRet<'a, Expr<'a>>> {
         if tokens.is_empty() {
-            return Some(ParseRet::Ok(Expr {
-                kind: ExprKind::Empty,
-                source: tokens,
-            }));
+            if allow_empty {
+                return Some(ParseRet::Ok(Expr {
+                    kind: ExprKind::Empty,
+                    source: tokens,
+                }));
+            } else {
+                return None;
+            }
         }
+
         //TODO: We may want to merge the callable parts here since names are going to be much more
         // common than binary operators and are much cheaper to parse first.
         Self::parse_num_expr(tokens)
@@ -1401,7 +1406,7 @@ impl<'a> Expr<'a> {
         let mut out = Vec::with_capacity(expr_sources.len());
 
         for tokens in expr_sources.iter() {
-            match Expr::parse(tokens) {
+            match Expr::parse(tokens, false) {
                 Some(Ok(expr)) => out.push(expr),
                 Some(SoftErr(expr, errs)) => {
                     out.push(expr);
@@ -1541,14 +1546,14 @@ impl<'a> Expr<'a> {
         let mut errors = Vec::new();
 
         let left = r#try!(
-            Expr::parse(left_tokens),
+            Expr::parse(left_tokens, false),
             left_tokens,
             errors,
             ErrorContext::BinOperLeft,
             ErrorSource::Single(op_token),
         );
         let right = r#try!(
-            Expr::parse(right_tokens),
+            Expr::parse(right_tokens, false),
             right_tokens,
             errors,
             ErrorContext::BinOperRight,
@@ -1610,7 +1615,7 @@ impl<'a> Expr<'a> {
             _ => return None,
         };
 
-        let rhs = match Expr::parse(&tokens[1..]) {
+        let rhs = match Expr::parse(&tokens[1..], false) {
             // We don't need to set EOF here because it's already going to the end of the set of
             // tokens we have available.
             Some(res) => res,
@@ -1694,7 +1699,7 @@ impl<'a> Expr<'a> {
         let mut errors = Vec::new();
 
         for field in fields.iter() {
-            let struct_field = match Expr::parse(field) {
+            let struct_field = match Expr::parse(field, false) {
                 // A valid expression was found, so this is an unnamed field
                 Some(expr) => {
                     // FIXME: We should actually set EOF here, but we don't currently have the
@@ -1733,7 +1738,7 @@ impl<'a> Expr<'a> {
 
                     // Try to parse the expression
                     let expr_tokens = &field[2..];
-                    let expr_pr = match Expr::parse(expr_tokens) {
+                    let expr_pr = match Expr::parse(expr_tokens, false) {
                         Some(expr_pr) => expr_pr,
                         None => {
                             // Error if there was not an expression

src/tokens/mod.rs
@@ -1,5 +1,6 @@
 use crate::errors::{self, PrettyError};
 use ansi_term::Color::Red;
+use std::fmt::{self, Display, Formatter, Write};
 use std::ops::Range;
 
 pub mod auto_sep;
@@ -150,90 +151,83 @@ pub enum TokenKind<'a> {
     Fake,
 }
 
-macro_rules! kwd {
-    ($($lit:expr => $variant:ident,)+) => {
-        #[derive(Debug, Copy, Clone, PartialEq)]
-        pub enum Keyword {
+macro_rules! exact {
+    (
+        $(#[$attrs:meta])*
+        pub enum $tyname:ident {
+            $($variant:ident => $lit:expr,)+
+        }
+    ) => {
+        $(#[$attrs])*
+        pub enum $tyname {
             $($variant,)+
         }
 
-        impl Keyword {
-            fn parse(keyword: &str) -> Option<Keyword> {
-                match keyword {
-                    $($lit => Some(Keyword::$variant),)+
+        impl $tyname {
+            fn parse(this: &str) -> Option<$tyname> {
+                match this {
+                    $($lit => Some($tyname::$variant),)+
                     _ => None,
                 }
             }
 
-            /// Returns the length of the keyword, in bytes/chars
+            /// Returns the length in bytes/chars
             fn len(&self) -> usize {
                 match self {
-                    $(Keyword::$variant => $lit.len(),)+
+                    $($tyname::$variant => $lit.len(),)+
+                }
+            }
+        }
+
+        impl Display for $tyname {
+            fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+                match self {
+                    $($tyname::$variant => write!(f, $lit),)+
                 }
             }
         }
     }
 }
 
-kwd! {
-    "fn" => Fn,
-    "if" => If,
-    "let" => Let,
-    "type" => Type,
-    "print" => Print,
-    "return" => Return,
-    "require" => Require,
-    "lemma" => Lemma,
+exact! {
+    #[derive(Debug, Copy, Clone, PartialEq)]
+    pub enum Keyword {
+        Fn => "fn",
+        If => "if",
+        Let => "let",
+        Type => "type",
+        Print => "print",
+        Return => "return",
+        Require => "require",
+        Lemma => "lemma",
+    }
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub enum Oper {
-    Add,
-    Sub,
-    Star,
-    Div,
-    Ref,
-    Assign,
-    Equals,
-    Lt,
-    LtOrEqual,
-    Gt,
-    GtOrEqual,
-    Not,
-    RightArrow,
-    Implies,
-    LongImplies,
-    LongImpliedBy,
-    Or,
-    And,
+exact! {
+    #[derive(Debug, Copy, Clone, PartialEq)]
+    pub enum Oper {
+        Add => "+",
+        Sub => "-",
+        Star => "*",
+        Div => "/",
+        Ref => "&",
+        Assign => "=",
+        Equals => "==",
+        Lt => "<",
+        LtOrEqual => "<=",
+        Gt => ">",
+        GtOrEqual => ">=",
+        Not => "!",
+        RightArrow => "->",
+        Implies => "=>",
+        LongImplies => "==>",
+        LongImpliedBy => "<==",
+        Or => "||",
+        And => "&&",
+    }
 }
 
 impl Oper {
-    fn parse(oper: &str) -> Option<Oper> {
-        use Oper::*;
-        match oper {
-            "+" => Some(Add),
-            "-" => Some(Sub),
-            "*" => Some(Star),
-            "/" => Some(Div),
-            "&" => Some(Ref),
-            "=" => Some(Assign),
-            "==" => Some(Equals),
-            "<" => Some(Lt),
-            "<=" => Some(LtOrEqual),
-            ">" => Some(Gt),
-            ">=" => Some(GtOrEqual),
-            "!" => Some(Not),
-            "->" => Some(RightArrow),
-            "=>" => Some(Implies),
-            "==>" => Some(LongImplies),
-            "<==" => Some(LongImpliedBy),
-            "||" => Some(Or),
-            "&&" => Some(And),
-            _ => None,
-        }
-    }
-
     /// returns true if newlines are allowed before this operator
     fn newline_prefix_allowed(self) -> bool {
         use Oper::*;
@@ -248,15 +242,6 @@ impl Oper {
     fn newline_postfix_allowed(self) -> bool {
         // right now, this is all of them
         true
-    }
-
-    fn len(&self) -> usize {
-        use Oper::*;
-        match self {
-            Add | Sub | Star | Div | Ref | Assign | Lt | Gt | Not => 1,
-            Equals | LtOrEqual | GtOrEqual | RightArrow | Implies | Or | And => 2,
-            LongImplies | LongImpliedBy => 3,
-        }
     }
 }
 
@@ -557,6 +542,19 @@ impl Token<'_> {
                 }
             }
             _ => (),
+        }
+    }
+}
+
+impl Display for Punc {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        use Punc::*;
+        match self {
+            Dot => write!(f, "."),
+            Comma => write!(f, ","),
+            Colon => write!(f, ":"),
+            Semi | Sep => write!(f, ";"),
+            Newline => write!(f, "newline"),
         }
     }
 }

src/ast/error.rs
@@ -1,7 +1,11 @@
 //! Error definitions for ast parsing
 
+use crate::errors;
 use crate::errors::PrettyError;
-use crate::tokens::Token;
+use crate::tokens::{Token, TokenKind};
+use ansi_term::Color::{Blue, Red};
+use std::fmt::Write;
+use std::ops::Range;
 
 /// Errors each have a kind and a context in which it occured. These can be combined with the
 /// source token to create a hopefully ok error message.
@@ -123,6 +127,174 @@ impl<'a> Error<'a> {
 
 impl PrettyError for Error<'_> {
     fn pretty_format(&self, file_str: &str, file_name: &str) -> String {
-        todo!()
+        use Kind::*;
+        use Source::{End, Single, EOF};
+
+        // Produces the description for a single token
+        fn token_desc(this: &Error, token: &Token) -> String {
+            use TokenKind::{
+                Curlys, Fake, InvalidChar, Keyword, NameIdent, Num, Oper, Parens, ProofLine, Punc,
+                Squares, TypeIdent,
+            };
+
+            match token.kind {
+                Keyword(kwd) => format!("keyword '{}'", kwd),
+                TypeIdent(s) => format!("type identifier '{}'", s),
+                NameIdent(s) => format!("identifier '{}'", s),
+                Num(s) => format!("numeric literal '{}'", s),
+
+                Parens(_) => "open parenthesis '('".into(),
+                Curlys(_) => "open curly-braces '{'".into(),
+                Squares(_) => "open square-brackets '['".into(),
+                ProofLine(_) => "proof line".into(),
+
+                // These two are deliberately left as not *quite* as descriptive - there's some
+                // mixing between these two categories, so they're just left here as generic
+                // "tokens". It should be fine.
+                Oper(o) => format!("token {}", o),
+                Punc(p) => format!("token {}", p),
+
+                // These two shouldn't occur at this point - parsing shouldn't have been given
+                // these sorts of tokens
+                InvalidChar(_) | Fake => panic!(
+                    "unexpected token {:?} in source for error {:?}",
+                    token, this
+                ),
+            }
+        }
+
+        // We'll build the error message in two parts. The first part is the main error message,
+        // which is determined by the error kind and context; the second is the main body, which is
+        // given only by the source.
+
+        // Pull yourself up by the bootstraps!
+        let mut help = None;
+
+        let (main_msg, needs_found): (&str, bool) = match (&self.kind, &self.context) {
+            (TypeIdent, _) => ("expected type identifier", true),
+            (ExpectingName, _) => ("expected idendentifier", true),
+            (ExpectingParens, _) => ("expected parenthetical", true),
+            (ExpectingCurlys, _) => ("expected curly braces", true),
+            (ExpectingKeyword, _) => ("expected keyword", true),
+            (ExpectingEquals, _) => ("expected '='", true),
+            (ExpectingExpr, _) => ("expected expression", true),
+            (ExpectingStmt, _) => ("expected statement", true),
+            (ExpectingType, _) => ("expected type", true),
+            (ExpectingIdent, _) => ("expected identifier", true),
+            (ExpectingColon, _) => ("expected ':'", true),
+            (MalformedStructField, _) => ("malformed struct field", false),
+            (UnexpectedToken, _) => ("unexpected token", true),
+            (IntegerValueTooLarge, _) => ("integer literal too large", false),
+            (SingleContractCondition, _) => {
+                // TODO: It would be great if this actually demonstrated to the user how to change
+                // their code to make it work
+                help = Some("help: to make a requirement, use the `require` keyword");
+                ("only single contract condition found", false)
+            }
+            (TooManyContractConditions(_), _) => ("cannot chain contract implication", false),
+            (CondWithoutCompareOp, _) => ("expected comparison operator", false),
+            // TODO: It would be great if this actually demonstrated to the user how to change
+            // their code to make it work
+            (ChainedComparison, _) => ("cannot chain comparison operators", false),
+            (ExpectingLemma, _) => {
+                help = Some("note: only lemmas are allowed within function bodies");
+                ("expected lemma", false)
+            }
+        };
+
+        // `byte_range` will be `Some` if the error was not at EOF, otherwise None
+        let (byte_range, found): (Option<Range<usize>>, String) = match &self.source {
+            Single(t) => (Some(t.byte_range().unwrap()), token_desc(self, t)),
+            Source::Range([t]) => (Some(t.byte_range().unwrap()), token_desc(self, t)),
+            Source::Range([t, .., end]) => (
+                Some(t.byte_range().unwrap().start..end.byte_range().unwrap().end),
+                token_desc(self, t),
+            ),
+            Source::Range([]) => panic!("AST error source with empty range: {:?}", self),
+            End(t) => {
+                let ch = match t.kind {
+                    TokenKind::Parens(_) => "')'",
+                    TokenKind::Curlys(_) => "'}'",
+                    TokenKind::Squares(_) => "']'",
+                    TokenKind::ProofLine(_) => "newline",
+                    _ => panic!(
+                        "end source specified for non-block token for error {:?}",
+                        self
+                    ),
+                };
+
+                let idx = t.byte_range().unwrap().end - 1;
+                (Some(idx..idx + 1), ch.into())
+            }
+            EOF => (None, "end of file".into()),
+        };
+
+        let (mut msg, spacing) = if let Some(range) = byte_range {
+            // ^ The error wasn't at EOF
+
+            let (context, spacing) = errors::context_lines_and_spacing(range, file_str, file_name);
+            let msg = if needs_found {
+                format!(
+                    "{}: {}, found {}\n{}\n",
+                    Red.paint("error"),
+                    main_msg,
+                    found,
+                    context,
+                )
+            } else {
+                format!("{}: {}\n{}\n", Red.paint("error"), main_msg, context,)
+            };
+
+            (msg, spacing)
+        } else {
+            // The error was at EOF. We'll display one extra line for context
+            let lines = file_str.lines().collect::<Vec<_>>();
+            let last_line_no = lines.len() - 1;
+
+            let mut msg = if needs_found {
+                format!("{}: {}, found {}\n", Red.paint("error"), main_msg, found)
+            } else {
+                format!("{}: {}\n", Red.paint("error"), main_msg)
+            };
+
+            // Add a little context line
+            let width = last_line_no.to_string().len();
+            let spacing = " ".repeat(width);
+            writeln!(msg, "{}{} {}:EOF", spacing, Blue.paint("-->"), file_name).unwrap();
+
+            let snd_last = lines.len().checked_sub(2).map(|i| lines[i]);
+
+            if let Some(line) = snd_last {
+                writeln!(
+                    msg,
+                    "{:>width$} {} {}",
+                    last_line_no - 1,
+                    Blue.paint("|"),
+                    line,
+                    width = width
+                )
+                .unwrap();
+            }
+
+            let last_line = lines.last().unwrap();
+            writeln!(msg, "{} {} {}", last_line_no, Blue.paint("|"), last_line).unwrap();
+            writeln!(
+                msg,
+                "{} {} {}",
+                spacing,
+                Blue.paint("|"),
+                errors::underline(&last_line, last_line.len()..last_line.len() + 1)
+            )
+            .unwrap();
+
+            (msg, spacing)
+        };
+
+        // And finally, we add the help messages
+        if let Some(help) = help {
+            writeln!(msg, "{} {} {}", spacing, Blue.paint("="), help).unwrap();
+        }
+
+        msg
     }
 }

src/errors.rs
@@ -60,7 +60,7 @@ pub fn line_info<'a>(file_str: &'a str, byte_idx: usize) -> (usize, usize, usize
 }
 
 /// Produces a row of caret characters to underline the given byte range of the line. The upper
-/// value on the byte range may be longer than the end of the line - this is quietly ignored.
+/// value on the byte range may be longer than the end of the line, plus one - this is quietly ignored.
 pub fn underline(line: &str, mut range: Range<usize>) -> String {
     range.end = range.end.min(line.len());
 
@@ -69,11 +69,15 @@ pub fn underline(line: &str, mut range: Range<usize>) -> String {
     // values.
 
     // For consistency, we'll double-check that the range is within the bounds of the line
-    assert!(range.start < line.len());
+    assert!(range.start <= line.len());
+
+    if range.end == range.start {
+        range.end += 1;
+    }
 
     let mut pre = " ".repeat(range.start);
     let mid = "^".repeat(range.end - range.start);
-    let post = " ".repeat(line.len() - range.end);
+    let post = " ".repeat(line.len().saturating_sub(range.end));
 
     write!(pre, "{}{}", mid, post).unwrap();
     pre

src/test_input.tc
@@ -7,7 +7,6 @@ fn hi(x: int) -> int {
 # require x >= 3
 # x <= 2 => 5 >= _
 fn foo(x: int, y: int) -> int {
-    # lemma 5 <= x
     bar(x, y, 3)
 }
 
@@ -17,7 +16,7 @@ fn bar(x: int, y: int, z: int) -> int {
 }
 
 fn main() {
-    print foo(1, 2);
+    3 +
 }
 
 fn t(my_struct: { hello: int, world: {x: int, y: int, z: bool} }) -> {a: int, b: bool, c: int} {

commit 0c6cc35583ee708fd6d733ac728104183e68759d

Fix parsing error introduced by 98b4fb8

src/ast/mod.rs
@@ -1097,7 +1097,7 @@ impl<'a> Stmt<'a> {
 
         // 4. Expr ";"
         let expr = next_option!(
-            Stmt::parse_terminated_expr(&tokens[3..]).unwrap_or_else(|| {
+            Stmt::parse_terminated_expr(&tokens[3..], false).unwrap_or_else(|| {
                 ParseRet::single_err(Error {
                     kind: ErrorKind::ExpectingExpr,
                     context: ErrorContext::LetExpr,
@@ -1144,7 +1144,7 @@ impl<'a> Stmt<'a> {
 
         let mut errors = Vec::new();
         let expr = next_option!(
-            Stmt::parse_terminated_expr(&tokens[1..]).unwrap_or_else(|| {
+            Stmt::parse_terminated_expr(&tokens[1..], false).unwrap_or_else(|| {
                 ParseRet::single_err(Error {
                     kind: ErrorKind::ExpectingExpr,
                     context: PrintExpr,
@@ -1208,7 +1208,7 @@ impl<'a> Stmt<'a> {
 
         // 3. Expr ";"
         let expr = next_option!(
-            Stmt::parse_terminated_expr(&tokens[2..]).unwrap_or_else(|| {
+            Stmt::parse_terminated_expr(&tokens[2..], false).unwrap_or_else(|| {
                 ParseRet::single_err(Error {
                     kind: ErrorKind::ExpectingExpr,
                     context: AssignExpr,
@@ -1274,7 +1274,7 @@ impl<'a> Stmt<'a> {
     fn parse_eval(tokens: &'a [Token<'a>]) -> Option<ParseRet<'a, Self>> {
         use StmtKind::Eval;
 
-        Some(Stmt::parse_terminated_expr(tokens)?.map(|expr| {
+        Some(Stmt::parse_terminated_expr(tokens, true)?.map(|expr| {
             // +1 for it being terminated
             let consumed = expr.consumed() + 1;
             Stmt {
@@ -1287,7 +1287,10 @@ impl<'a> Stmt<'a> {
     /// Extracts a semicolon-terminated expression from a subset of the given tokens, returning the
     /// expression. The number of consumed tokens will always be equal to the number consumed by
     /// the expression, plus one.
-    fn parse_terminated_expr(tokens: &'a [Token<'a>]) -> Option<ParseRet<'a, Expr<'a>>> {
+    fn parse_terminated_expr(
+        tokens: &'a [Token<'a>],
+        allow_empty: bool,
+    ) -> Option<ParseRet<'a, Expr<'a>>> {
         use tokens::{Punc::Sep, TokenKind::Punc};
 
         let sep_idx = tokens
@@ -1296,7 +1299,7 @@ impl<'a> Stmt<'a> {
             .find(|(_, t)| t.kind == Punc(Sep))
             .map(|(i, _)| i)?;
 
-        Expr::parse(&tokens[..sep_idx], false).map(|ret| match ret {
+        Expr::parse(&tokens[..sep_idx], allow_empty).map(|ret| match ret {
             ParseRet::Ok(expr) => ParseRet::Ok(expr),
             ParseRet::SoftErr(expr, errs) => ParseRet::SoftErr(
                 expr,